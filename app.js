bec636dc40f1c4bc0756cd25c4b4a6cee9958e92
Chris Lerum <chris.lerum@gmail.com>
2015-03-31 06:04:38 -0400

    x

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ angular.module("Bookmarks", [])
     {id: 3, name: 'Kids'}
   ]
   $scope.bookmarks = [
-    {id: 0, name: 'AngularJS', url: 'https://angularjs.org/', category: 'Work'},
-    {id: 1, name: 'Google', url: 'https://google.com', category: 'Work'},
-    {id: 2, name: 'Turtles', url: 'https://www.google.com/search?q=turtles', category: 'Play'},
-    {id: 3, name: 'Georgia', url: 'https://en.wikipedia.org/wiki/Georgia_(U.S._state)', category: 'Art'},
-    {id: 4, name: 'Google News', url: 'https://news.google.com/', category: 'Play'},
+    {id: 0, title: 'AngularJS', url: 'https://angularjs.org/', category: 'Work'},
+    {id: 1, title: 'Google', url: 'https://google.com', category: 'Work'},
+    {id: 2, title: 'Turtles', url: 'https://www.google.com/search?q=turtles', category: 'Play'},
+    {id: 3, title: 'Georgia', url: 'https://en.wikipedia.org/wiki/Georgia_(U.S._state)', category: 'Art'},
+    {id: 4, title: 'Google News', url: 'https://news.google.com/', category: 'Play'},
   ]
 
   $scope.currentCategory = null;
@@ -26,5 +26,35 @@ angular.module("Bookmarks", [])
 
   $scope.isCurrentCategory = function(category) {
     return $scope.currentCategory && $scope.currentCategory.name === category.name
+  }
+
+  $scope.isCreating = false;
+  $scope.isEditing = false;
+
+  $scope.startCreating = function(){
+    console.log('start creating')
+    $scope.isCreating = true;
+    $scope.isEditing = false;
+  }
+
+  $scope.startEditing = function(){
+    $scope.isCreating = false;
+    $scope.isEditing = true;
+  }
+
+  $scope.cancelCreating = function(){
+    $scope.isCreating = false;
+  }
+
+  $scope.cancelEditing = function(){
+    $scope.isEditing = false;
+  }
+
+  $scope.shouldShowCreating = function(){
+    return $scope.currentCategory && !$scope.isEditing;
+  }
+
+  $scope.shouldShowEditing = function(){
+    return $scope.isEditing && !$scope.isCreating;
   }
 });
